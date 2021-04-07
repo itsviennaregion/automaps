b2d1b8caeb3858c7bca21284c8150c93999fff5e
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-04-07 14:10:46 +0200

    Implement basic download mechanism

--- app.py
+++ app.py
@@ -1,46 +1,45 @@
 import os
 import time
 
-import pandas as pd
-from sqlalchemy import create_engine
 import streamlit as st
 
-from automaps.db import get_engine
+from automaps.fileserver import download_button
 from automaps.generators import MapGenerator
 from automaps.maptype import MapType
 from automaps.selector import SelectorSQL, SelectorSimple
+
+
+BASEPATH_FILESERVER = r"D:\temp\automap"
 
 
 class MapGeneratorUeberblick(MapGenerator):
     name = "ÖV-Überblick"
 
     def _set_steps(self):
-        self.steps = [
-            ("Schritt 1", self.schritt_1),
-            ("Schritt 2", self.schritt_2)
-        ]
+        self.steps = [("Schritt 1", self.schritt_1), ("Schritt 2", self.schritt_2)]
 
     def schritt_1(self):
         time.sleep(1)
-        
+
     def schritt_2(self):
         time.sleep(2)
-        
+        with open(self.filename, "w") as f:
+            f.write(f"Ich bin ein ÖV-Überblick mit Daten: {self.data}\n")
+
 
 class MapGeneratorPendler(MapGenerator):
     name = "Pendler"
 
     def _set_steps(self):
-        self.steps = [
-            ("Schritt A", self.schritt_1),
-            ("Schritt B", self.schritt_2)
-        ]
+        self.steps = [("Schritt A", self.schritt_A), ("Schritt B", self.schritt_B)]
 
-    def schritt_1(self):
+    def schritt_A(self):
         time.sleep(1)
-        
-    def schritt_2(self):
+
+    def schritt_B(self):
         time.sleep(2)
+        with open(self.filename, "w") as f:
+            f.write(f"Ich bin eine Pendlerkarte mit Daten: {self.data}\n")
 
 
 MAPTYPES_AVAIL = {
@@ -72,15 +71,29 @@ MAPTYPES_AVAIL = {
 
 def start_frontend():
     maptype = MAPTYPES_AVAIL[st.sidebar.radio("Kartentyp", list(MAPTYPES_AVAIL.keys()))]
+    
     st.write(f"# {maptype.name}")
+    
     selector_values = {}
     for s in maptype.selectors:
         selector_values[s.label] = s.widget
+        
     for k, v in selector_values.items():
         st.write(f"{k}: __{v}__")
+        
     if st.button("Karte erstellen"):
-        maptype.generator(data=selector_values).generate()
+        filename = maptype.generator(
+            data=selector_values, basepath_fileserver=BASEPATH_FILESERVER
+        ).generate()
+
+        with open(filename, "rb") as f:
+            s = f.read()
+        download_button_str = download_button(
+            s, os.path.basename(filename), f"Download {os.path.basename(filename)}"
+        )
+        st.markdown(download_button_str, unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
+
     start_frontend()
--- automaps/generators.py
+++ automaps/generators.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import os
 from typing import Callable, List, Tuple
 
 import streamlit as st
@@ -7,26 +8,27 @@ import streamlit as st
 class MapGenerator(ABC):
     name: str
     steps: List[Tuple[str, Callable]] = []
-    
-    def __init__(self, data: dict):
+
+    def __init__(self, data: dict, basepath_fileserver: str):
         self.data = data
+        self.basepath_fileserver = basepath_fileserver
         self._set_steps()
-    
-    def generate(self):
-        print(f"{self.name} start")
+
+    def generate(self) -> str:
         with st.spinner(f"Erstelle Karte {self.name} {self.data} ..."):
             for name, func in self.steps:
                 print(name, func)
                 with st.spinner(name):
                     func()
-        print(f"{self.name} fertig")
         st.success(f"{self.name} fertig")
-    
+        return self.filename
+
+    @property
+    def filename(self):
+        return os.path.join(
+            self.basepath_fileserver, f"{self.name}_{'_'.join(self.data.values())}.txt"
+        )
+
     @abstractmethod
     def _set_steps(self):
         pass
-        
-
-    
-
-
