f48842ef5e85d41869e94b12a766c44aacab513f
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-05-10 15:35:34 +0200

    Add new options to ÖV-Überblick

--- conf.py
+++ conf.py
@@ -18,6 +18,14 @@ MAPTYPES_AVAIL: Dict[str, MapType] = {
                 st.selectbox,
                 no_value_selected_text="Räumliche Ebene auswählen ...",
             ),
+
+            # Räumliche Ebene: Linie
+            SelectorSimple(
+                "Bus oder Bahn",
+                ["Bus", "Bahn"],
+                st.radio,
+                depends_on_selectors={"Räumliche Ebene": "Linie"},
+            ),
             SelectorSQL(
                 "Linie",
                 "select distinct liniennummer from geom_mabinso_strecken",
@@ -25,6 +33,20 @@ MAPTYPES_AVAIL: Dict[str, MapType] = {
                 no_value_selected_text="Liniennummer auswählen ...",
                 depends_on_selectors={"Räumliche Ebene": "Linie"},
             ),
+            SelectorSimple(
+                "Linie oder Kurs",
+                ["Linie", "Kurs"],
+                st.radio,
+                depends_on_selectors={"Räumliche Ebene": "Linie"},
+            ),
+            SelectorSQL(
+                "Tagesart",
+                "select distinct tagesart from geom_mabinso_strecken",
+                st.selectbox,
+                depends_on_selectors={"Linie oder Kurs": "Kurs"}
+            ),
+
+            # Räumliche Ebene: Gemeinde
             SelectorSQL(
                 "Gemeinde",
                 "select distinct gem_name from gem",
@@ -35,16 +57,19 @@ MAPTYPES_AVAIL: Dict[str, MapType] = {
         ],
         print_layout="test_layout",
     ),
+
+
     "Test": MapType(
         name="Test",
-        description="Hier kann man alles mögliche testen. " "Das funktioniert so: ...",
+        description="Hier kann man alles mögliche testen.",
         selectors=[
             SelectorSimple("Layout", ["A", "B"], st.radio),
-            SelectorSimple("Ebene", ["", "Gemeinde", "Bezirk"], st.selectbox),
+            SelectorSimple("Ebene", ["Gemeinde", "Bezirk"], st.selectbox),
             SelectorSQL(
                 "Gemeinde",
                 "select distinct gem_name from gem",
                 st.selectbox,
+                no_value_selected_text="...???...",
                 depends_on_selectors={"Ebene": "Gemeinde", "Layout": "A"},
             ),
             SelectorSQL(
