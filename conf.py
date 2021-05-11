87eba0b9e4d889d18397bdbff45685790e24a79a
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-05-11 15:14:21 +0200

    Add option file format

--- conf.py
+++ conf.py
@@ -118,6 +118,11 @@ MAPTYPES_AVAIL: Dict[str, MapType] = {
                 ],
                 st.radio,
             ),
+            SelectorSimple(
+                "Dateiformat",
+                ["PDF"],
+                st.radio
+            ),
         ],
         print_layout="test_layout",
     ),
