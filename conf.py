6190f77be385d015a6d67607901c227eca189910
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-05-11 14:46:14 +0200

    Add new config options

--- conf.py
+++ conf.py
@@ -10,28 +10,70 @@ MAPTYPES_AVAIL: Dict[str, MapType] = {
     "ÖV-Überblick": MapType(
         name="ÖV-Überblick",
         description="Hier kann man eine ÖV-Überblickskarte erstellen. "
-        "Das funktioniert so: ...",
+        "Aber derzeit nur __testweise__. Sinnvolle Ergebnisse werden nur mit der "
+        "__Räumlichen Ebene 'Gemeinde'__ erzeugt.",
         selectors=[
+            (st.write, "## Grundeinstellungen"),
+            SelectorSimple("Fahrplanversion", ["aktueller Fahrplan"], st.selectbox),
             SelectorSimple(
                 "Räumliche Ebene",
-                ["Linie", "Gemeinde"],
+                [
+                    "Haltestelle",
+                    "Linie",
+                    "Ort/Stadt",
+                    "Gemeinde",
+                    "Bezirk",
+                    "Bundesland",
+                    "Ausschreibungsregion",
+                ],
                 st.selectbox,
-                widget_args={"help": "Hier könnte Ihr Hilfetext stehen!"},
+                widget_args={"help": "Hier könnte __Ihr__ Hilfetext stehen!"},
                 no_value_selected_text="Räumliche Ebene auswählen ...",
+            ),
+            (st.write, "## Datenlayer"),
+            # Räumliche Ebene: Haltestelle
+            SelectorSQL(
+                "Haltestelle",
+                "select distinct vonhstname from geom_mabinso_strecken",
+                st.multiselect,
+                depends_on_selectors={"Räumliche Ebene": "Haltestelle"},
             ),
             # Räumliche Ebene: Linie
             SelectorSimple(
-                "Bus oder Bahn",
+                "Bus und/oder Bahn",
                 ["Bus", "Bahn"],
-                st.radio,
+                st.multiselect,
                 depends_on_selectors={"Räumliche Ebene": "Linie"},
             ),
             SelectorSQL(
-                "Linie",
+                "Linie Bus",
+                "select distinct liniennummer from geom_mabinso_strecken "
+                "where liniennummer::int < 300",
+                st.selectbox,
+                no_value_selected_text="Liniennummer auswählen ...",
+                depends_on_selectors={"Bus und/oder Bahn": ["Bus"]},
+            ),
+            SelectorSQL(
+                "Linie Bahn",
+                "select distinct liniennummer from geom_mabinso_strecken "
+                "where liniennummer::int >= 300",
+                st.selectbox,
+                no_value_selected_text="Liniennummer auswählen ...",
+                depends_on_selectors={"Bus und/oder Bahn": ["Bahn"]},
+            ),
+            SelectorSQL(
+                "Linie Bus/Bahn",
                 "select distinct liniennummer from geom_mabinso_strecken",
                 st.selectbox,
                 no_value_selected_text="Liniennummer auswählen ...",
-                depends_on_selectors={"Räumliche Ebene": "Linie"},
+                depends_on_selectors={"Bus und/oder Bahn": ["Bus", "Bahn"]},
+            ),
+            SelectorSQL(
+                "Linie Bus/Bahn",
+                "select distinct liniennummer from geom_mabinso_strecken",
+                st.selectbox,
+                no_value_selected_text="Liniennummer auswählen ...",
+                depends_on_selectors={"Bus und/oder Bahn": ["Bahn", "Bus"]},
             ),
             SelectorSimple(
                 "Linie oder Kurs",
@@ -52,6 +94,29 @@ MAPTYPES_AVAIL: Dict[str, MapType] = {
                 st.selectbox,
                 no_value_selected_text="Gemeinde auswählen ...",
                 depends_on_selectors={"Räumliche Ebene": "Gemeinde"},
+            ),
+            SelectorSimple(
+                "Sonstige Objekte",
+                ["Schulen", "Siedlungskerne"],
+                st.multiselect,
+                widget_args={"default": ["Schulen", "Siedlungskerne"]},
+            ),
+            (st.write, "## Layout"),
+            SelectorSimple(
+                "Kartendarstellung", ["extern", "intern", "reduziert"], st.radio
+            ),
+            SelectorSimple("Ausrichtung", ["Querformat", "Hochformat"], st.radio),
+            SelectorSimple("Stil", ["Corporate Design"], st.radio),
+            SelectorSimple("Format", ["A4", "A0"], st.radio),
+            SelectorSimple(
+                "Grundkarte",
+                [
+                    "basemap.at Standard",
+                    "basemap.at Grau",
+                    "basemap.at Stumm",
+                    "OpenStreetMap",
+                ],
+                st.radio,
             ),
         ],
         print_layout="test_layout",
