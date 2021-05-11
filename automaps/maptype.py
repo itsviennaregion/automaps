321b946f0621a532bc8c951bfbd4697ccf4f3ee6
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-05-11 14:45:49 +0200

    Add st.write

--- automaps/maptype.py
+++ automaps/maptype.py
@@ -1,5 +1,7 @@
 from typing import Iterable
 
+import streamlit as st
+
 from automaps.selector import BaseSelector
 
 
@@ -23,19 +25,32 @@ class MapType:
         selector_values = {}
         has_init_values = False
         for s in self.selectors:
-            if not s.depends_on_selectors:
-                selector_values[s.label] = s.widget
-            else:
-                show_widget = True
-                for sel_name, sel_value in s.depends_on_selectors.items():
-                    if selector_values.get(sel_name, None) != sel_value:
-                        show_widget = False
-                if show_widget:
+            if isinstance(s, BaseSelector):
+                if not s.depends_on_selectors:
                     selector_values[s.label] = s.widget
                 else:
-                    selector_values[s.label] = None
-            if selector_values[s.label] == s.no_value_selected_text:
-                has_init_values = True
+                    show_widget = True
+                    for sel_name, sel_value in s.depends_on_selectors.items():
+                        if selector_values.get(sel_name, None) != sel_value:
+                            show_widget = False
+                    if show_widget:
+                        selector_values[s.label] = s.widget
+                    else:
+                        selector_values[s.label] = None
+                if selector_values[s.label] == s.no_value_selected_text:
+                    has_init_values = True
+            elif isinstance(s, tuple):
+                try:
+                    name = s[0].__name__
+                    if name == "write":
+                        s[0](s[1])
+                    else:
+                        st.error(
+                            f"'{name}' nicht unterstützt! Bitte 'st.write' "
+                            "verwenden."
+                        )
+                except Exception as e:
+                    st.error(e)
         if has_init_values:
             selector_values["has_init_values"] = True
         return selector_values
