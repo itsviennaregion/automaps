1d0fcce821d304a51021052406d50b853aac8b32
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-05-12 12:49:43 +0200

    Update ueberblick_gebiet.py

--- automaps/generators/ueberblick_gebiet.py
+++ automaps/generators/ueberblick_gebiet.py
@@ -9,28 +9,29 @@ class MapGeneratorUeberblickGebiet(MapGenerator):
     def _set_steps(self):
         self.steps = OrderedDict(
             {
-                "Projekt laden": Step(self.load_project, 0.5),
+                "Projekt laden": Step(self.load_project, 1),
                 "Layer filtern": Step(self.filter_layers, 1),
                 "Kartenausschnitt festlegen": Step(self.set_extent, 1),
-                "Karte exportieren": Step(self.export_layout, 0.5),
+                "Karte exportieren": Step(self.export_layout, 5),
             }
         )
 
     def load_project(self):
         self._set_project_variable("data", str(self.data))
+        self._set_project_variable("gemeinde_aktiv", self.data["Gemeinde"])
 
     def filter_layers(self):
-        self._set_map_layer_filter_expression(
-            "bev_gemeinden", f"pg = '{self.data['Gemeinde']}'"
-        )
+        if self.data["Gemeinde"]:
+            self._set_map_layer_filter_expression(
+                "bev_gemeinden", f"pg = '{self.data['Gemeinde']}'"
+            )
+
         linien = self.data["Linien in der Gemeinde"] or self.data["Linien im Bezirk"]
-        print(linien)
         if "ALLE" not in linien:
             self._set_map_layer_filter_expression(
-                "Linien",
-                f"lineefa in {linien}".replace("[", "(").replace("]", ")")
+                "Linien", f"lineefa in {linien}"
             )
-        self._set_project_variable("gemeinde_aktiv", self.data["Gemeinde"])
+
         self._set_map_layer_visibility(self.data["Grundkarte"], True)
 
     def set_extent(self):
