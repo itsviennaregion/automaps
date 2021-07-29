7ed7b1dc5a29216751c5b0a082dfa16978cfeb11
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-07-29 12:00:38 +0200

    Draft basic idea

---
+++ idee.py
@@ -0,0 +1,13 @@
+from automaps import AutoMaps
+
+import conf
+
+app = AutoMaps(conf)
+
+# Aufruf mit python idee.py
+# alternativ mit CLI: automaps idee.py (ev. mit Parametern)
+# app startet frontend und server als sub processes
+# conf wird von user geschrieben, kann aus mehreren py-Files bestehen
+# conf beinhaltet alles aus conf_local, conf_server, conf und db.ini
+# conf beinhaltet streamlit-Abteilung, Optionen werden an Streamlit weitergereicht
+
