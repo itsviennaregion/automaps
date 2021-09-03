fd6d79e75f215482fc1ce67a483574f88d785902
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-09-03 14:46:58 +0200

    Remove comment

--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -12,11 +12,6 @@ def test_db_setup(postgresql):
     assert result == [(1, "Achau"), (2, "Traiskirchen")]
 
 
-# def test_get_engine(mocker, postgresql):
-#     engine = create_engine("postgresql+psycopg2://", creator=postgresql)
-#     mocker.patch.object(automaps.db, "get_engine", return_value=engine)
-
-
 def test_get_engine(mock_engine):
     engine = automaps.db.get_engine()
     result = list(engine.execute(text("SELECT id, name FROM cities")))
