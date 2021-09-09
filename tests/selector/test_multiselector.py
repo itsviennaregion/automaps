c32825dc792d9b7a9e933c9c1db240ce73e54d57
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-09-09 10:48:26 +0200

    Add tests for MultiSelector

---
+++ tests/selector/test_multiselector.py
@@ -0,0 +1,9 @@
+from automaps.selector import MultiSelector, SelectorSimple, SelectorSQL
+
+def test_multiselector():
+    sel_simple = SelectorSimple("sel_simple", [500, 530, 42])
+    sel_sql = SelectorSQL("sel_sql", "select distinct name from cities")
+    sel = MultiSelector("sel_multi", [sel_simple, sel_sql])
+    assert len(sel.selectors) == 2
+    assert sel.selectors[0].label == "sel_simple"
+    assert sel.selectors[1].label == "sel_sql"
