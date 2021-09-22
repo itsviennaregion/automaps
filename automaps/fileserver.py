56a5fc812a1b362452f7e9c1da44ed8c33ee5312
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-09-22 08:48:40 +0200

    Style download button

--- automaps/fileserver.py
+++ automaps/fileserver.py
@@ -18,32 +18,40 @@ def download_button(download_filepath, button_text):
     custom_css = f"""
         <style>
             #{button_id} {{
-                background-color: rgb(230, 230, 230);
-                color: rgb(38, 39, 48);
+                background-color: #99cc00;
+                color: rgba(0,0,0,0.87);
+                border: 0;
                 padding: 0.25em 0.38em;
                 position: relative;
                 text-decoration: none;
-                border-radius: 4px;
-                border-width: 1px;
-                border-style: solid;
-                border-color: rgb(230, 234, 241);
-                border-image: initial;
+                border-radius: 0.25rem;
             }}
             #{button_id}:hover {{
-                border-color: rgb(246, 51, 102);
-                color: rgb(246, 51, 102);
+                background-color: #649b00;
+                color: rgba(0,0,0,0.87);
+                border: 0;
+                border-radius: 0.25rem;
             }}
             #{button_id}:active {{
-                box-shadow: none;
-                background-color: rgb(246, 51, 102);
-                color: white;
+                background-color: #99cc00;
+                color: rgba(0,0,0,0.87);
+                border: 0;
+                border-radius: 0.25rem;
+                }}
+            #{button_id}:focus:not(:active) {{
+                background-color: #99cc00;
+                color: rgba(0,0,0,0.87);
+                border: 0;
+                border-radius: 0.25rem;
                 }}
         </style> """
 
     dl_link = (
         custom_css
+        + '<div class="row-widget stButton">'
         + f'<a download="{filename}" id="{button_id}" '
         + f'href="downloads/{filename}">{button_text}</a><br></br>'
+        + "</div>"
     )
 
     return dl_link
