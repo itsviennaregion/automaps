9ce17ac500027a771169c5a9b0bdeee962bf0996
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-09-23 15:52:45 +0200

    Add config stubs

--- automaps/automapsconf.py
+++ automaps/automapsconf.py
@@ -1,11 +1,20 @@
-from typing import Dict
+from typing import List
 
-# from automaps.maptype import MapType
+from automaps.maptype import MapType
 
 SHOW_DEBUG_INFO: bool
 PORT: int
+QGIS_INSTALLATION_PATH: str
+BASEPATH_FILESERVER: str
 LOGO_PATH: str
 PROJECT_TITLE: str
+MAPTYPE_TEXT: str
+MISSING_ATTRIBUTES_TEXT: str
+WAITING_FOR_SERVER_TEXT: str
+SPINNER_TEXT: str
+MAP_READY_TEXT: str
 CUSTOM_HTML: str
-MAPTYPES_AVAIL: list
+DOWNLOAD_BUTTON_STYLE: str
+FILEPATH_QGIS_PROJECT: str
+MAPTYPES_AVAIL: List[MapType]
 db: dict
