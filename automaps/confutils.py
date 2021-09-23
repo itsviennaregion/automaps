5f2eceafd2564ceaaf46b5146f4d37bbdcd4856f
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-09-23 15:44:40 +0200

    Implement get_config_option

--- automaps/confutils.py
+++ automaps/confutils.py
@@ -1,4 +1,14 @@
+from typing import Any
+
 import automapsconf
 
 def has_config_option(config_option: str) -> bool:
     return hasattr(automapsconf, config_option) and getattr(automapsconf, config_option)
+
+def get_config_value(config_option: str, default_value: Any = None) -> Any:
+    if has_config_option(config_option):
+        return getattr(automapsconf, config_option)
+    elif default_value:
+        return default_value
+    else:
+        return None
