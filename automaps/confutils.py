3e314e0455ad77ace03956fea8f9f7d1aed8176c
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-09-23 15:47:01 +0200

    Reformat

--- automaps/confutils.py
+++ automaps/confutils.py
@@ -2,8 +2,10 @@ from typing import Any
 
 import automapsconf
 
+
 def has_config_option(config_option: str) -> bool:
     return hasattr(automapsconf, config_option) and getattr(automapsconf, config_option)
+
 
 def get_config_value(config_option: str, default_value: Any = None) -> Any:
     if has_config_option(config_option):
