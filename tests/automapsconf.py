baa50b1d3f4e88e3690c31ee3e7050fa03af6d1a
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2021-09-29 15:46:43 +0200

    Add missing config options

--- tests/automapsconf.py
+++ tests/automapsconf.py
@@ -1,7 +1,21 @@
-from typing import Dict
+from typing import List
 
-# from automaps.maptype import MapType
+from automaps.maptype import MapType
 
+SHOW_DEBUG_INFO: bool
 PORT_MAP_SERVER: int
-MAPTYPES_AVAIL: list
+QGIS_INSTALLATION_PATH: str
+BASEPATH_FILESERVER: str
+DOWNLOADS_RETAIN_TIME: int
+LOGO_PATH: str
+PROJECT_TITLE: str
+MAPTYPE_TEXT: str
+MISSING_ATTRIBUTES_TEXT: str
+WAITING_FOR_SERVER_TEXT: str
+SPINNER_TEXT: str
+MAP_READY_TEXT: str
+CUSTOM_HTML: str
+DOWNLOAD_BUTTON_STYLE: str
+FILEPATH_QGIS_PROJECT: str
+MAPTYPES_AVAIL: List[MapType]
 db: dict = {}
