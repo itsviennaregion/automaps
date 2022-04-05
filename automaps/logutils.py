705f12a528d45b5c77eac80a69fff2724c70e198
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2022-04-05 14:17:29 +0200

    Refactor and clean up

--- automaps/logutils.py
+++ automaps/logutils.py
@@ -5,22 +5,24 @@ from uuid import UUID
 
 from automaps.confutils import get_config_value
 
-BASIC_FORMAT = "%(asctime)s -- %(levelname)-7s -- %(name)s -- %(message)s"
 
-FORMATTER_LOGFILE = logging.Formatter(get_config_value("LOG_FORMAT", BASIC_FORMAT))
-
-
-def add_file_handler(logger_):
+def add_file_handler(logger: logging.Logger) -> bool:
     if get_config_value("LOG_PATH"):
+        format_logfile = get_config_value(
+            "LOG_FORMAT", "%(asctime)s -- %(levelname)-7s -- %(name)s -- %(message)s"
+        )
+        formatter_logfile = logging.Formatter(format_logfile)
         fh = logging.handlers.TimedRotatingFileHandler(
-            get_config_value("LOG_PATH"), when="W0"
+            get_config_value("LOG_PATH"), when="W0"  # TODO: automapsconf.py
         )
         fh.setLevel(get_config_value("LOG_LEVEL_SERVER", logging.INFO))
-        fh.setFormatter(FORMATTER_LOGFILE)
-        logger_.addHandler(fh)
+        fh.setFormatter(formatter_logfile)
+        logger.addHandler(fh)
+        return True
+    return False
 
 
-def shorten_uuid(uuid: Union[UUID, str], extra_short: Optional[bool] = False):
+def shorten_uuid(uuid: Union[UUID, str], extra_short: Optional[bool] = False) -> str:
     uuid = str(uuid)
     try:
         prefix = "" if extra_short else uuid.split("-")[0]
