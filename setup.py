92f69c41016e6d92664fea488504788467cf970a
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2022-04-19 14:44:34 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="automaps",
-    version="0.9.1",
+    version="0.9.2",
     author="Roland Lukesch",
     author_email="roland.lukesch@its-viennaregion.at",
     description="Making maps with streamlit and QGIS",
@@ -15,23 +15,22 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://gitlab.com/its-vienna-region/digilab/automatisierte-karten",
     install_requires=[
-        "Jinja2>=3.0.1",
-        "pandas>=1.2.4",
-        "psycopg2>=2.8.4",
-        "streamlit>=0.80.0",
-        "SQLAlchemy>=1.4.7",
-        "Pillow>=8.3.1",
-        "pyzmq>=22.0.3",
+        "click>=8.0.4",
+        "hypothesis>=6.43.3",
+        "Jinja2>=3.1.1",
+        "pandas>=1.4.2",
+        "psycopg2>=2.9.3",
+        "pytest>=7.1.1",
+        "streamlit>=1.8.1",
+        "SQLAlchemy>=1.4.35",
+        "Pillow>=9.1.0",
+        "pyzmq>=22.3.0",
     ],
     packages=setuptools.find_packages(),  # ["automaps", "automaps.client", "automaps.generators", "automaps.server", "automaps.scripts"],
     python_requires=">=3.8",
-    package_data={
-        "automaps":
-            ["data/demo/*",
-             "data/demo/.streamlit/*"]
-    },
+    package_data={"automaps": ["data/demo/*", "data/demo/.streamlit/*"]},
     entry_points="""
         [console_scripts]
         automaps=automaps.scripts.project:cli
-    """
+    """,
 )
