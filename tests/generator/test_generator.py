0b3ba7ed1b7bb369b59417172a8ad51379b60cb8
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2022-05-19 14:14:15 +0200

    Add tests

--- tests/generator/test_generator.py
+++ tests/generator/test_generator.py
@@ -1,9 +1,13 @@
 from collections import OrderedDict
+from mmap import MADV_HUGEPAGE
 import os
+from pathlib import Path
 from typing import get_args
 
 import pytest
-from qgis.core import QgsPrintLayout, QgsProject
+from qgis.core import QgsLayerTreeNode, QgsMapLayer, QgsPrintLayout, QgsProject
+
+from automaps.fileserver import get_streamlit_download_path
 
 import automapsconf
 
@@ -14,7 +18,14 @@ automapsconf.FILEPATH_QGIS_PROJECT = PATH
 from automaps.generators.base import MapGenerator, Step, StepData
 
 
-DATA = {"city": "Achau", "export_format": "pdf", "funny_value": 123123123123}
+DATA = OrderedDict(
+    {
+        "city": "Achau",
+        "!FILEFORMAT!": "pdf",
+        "funny_value": 123123123123,
+        "numbers": [1, 2, 3],
+    }
+)
 
 
 class MyMapGenerator(MapGenerator):
@@ -29,7 +40,7 @@ class MyMapGenerator(MapGenerator):
         )
 
     def initialize(self):
-        pass
+        self._init_layers()
 
     def set_variables(self):
         self._set_project_variable("city", self.data["city"])
@@ -37,6 +48,11 @@ class MyMapGenerator(MapGenerator):
 
 class MyFancyMapGenerator(MyMapGenerator):
     data_to_exclude_from_filename = ["funny_value"]
+
+
+@pytest.fixture
+def g(tmp_path) -> MapGenerator:
+    return MyMapGenerator(DATA, tmp_path, "myLayout", StepData({}), "abc123")
 
 
 def test_init(tmp_path):
@@ -52,36 +68,148 @@ def test_init(tmp_path):
     assert g.file_format == "pdf"
 
 
+def test_filename(g: MapGenerator):
+    assert (
+        Path(g.filename).name
+        == "abc123_My_Map_Generator_Achau_123123123123_[1,_2,_3].pdf"
+    )
+
+
 def test_exclude_from_filename(tmp_path):
     g = MyFancyMapGenerator(DATA, tmp_path, "myLayout", StepData({}), "abc123")
-    assert str(g.data["funny_value"]) not in g.filename
+    assert str(DATA["funny_value"]) not in g.filename
 
 
-def test_run_step(tmp_path):
+def test_file_format(tmp_path):
     g = MyMapGenerator(DATA, tmp_path, "myLayout", StepData({}), "abc123")
+    assert g.file_format == "pdf"
+    g = MyMapGenerator(
+        DATA, tmp_path, "myLayout", StepData({}), "abc123", default_file_format="svg"
+    )
+    assert g.file_format == "svg"
+    DATA["!FILEFORMAT!"] = "pdf"
+    g = MyMapGenerator(DATA, tmp_path, "myLayout", StepData({}), "abc123")
+    assert g.file_format == "pdf"
+    DATA["!FILEFORMAT!"] = "png"
+    g = MyMapGenerator(DATA, tmp_path, "myLayout", StepData({}), "abc123")
+    assert g.file_format == "png"
+    DATA["!FILEFORMAT!"] = "PNG"
+    g = MyMapGenerator(DATA, tmp_path, "myLayout", StepData({}), "abc123")
+    assert g.file_format == "png"
+    with pytest.raises(ValueError):
+        DATA["!FILEFORMAT!"] = "bmp"
+        g = MyMapGenerator(DATA, tmp_path, "myLayout", StepData({}), "abc123")
+
+
+def test_get_project(g: MapGenerator):
+    assert isinstance(g._get_project(), QgsProject)
+
+
+def test_init_layers(g: MapGenerator):
+    subset_strings = [lyr.subsetString() for lyr in g._get_all_map_layers()]
+    visibilities = [
+        node.itemVisibilityChecked()
+        for node in g.step_data.project.layerTreeRoot().children()
+        if isinstance(node, QgsLayerTreeNode)
+    ]
+    assert any(len(s) > 0 for s in subset_strings)
+    assert any(v for v in visibilities)
+
+    g.run_step("Initialize")
+    subset_strings = [lyr.subsetString() for lyr in g._get_all_map_layers()]
+    visibilities = [
+        node.itemVisibilityChecked()
+        for node in g.step_data.project.layerTreeRoot().children()
+        if isinstance(node, QgsLayerTreeNode)
+    ]
+    assert all([s == "" for s in subset_strings])
+    assert all([not v for v in visibilities])
+
+
+def test_set_project_variable(g: MapGenerator):
     g.run_step("Initialize")
     assert g.step_data.project.customVariables() == {}
     g.run_step("Set variables")
     assert g.step_data.project.customVariables() == {"city": g.data["city"]}
 
 
-def test_file_format(tmp_path):
-    data = {}
-    g = MyMapGenerator(data, tmp_path, "myLayout", StepData({}), "abc123")
-    assert g.file_format == "pdf"
-    g = MyMapGenerator(
-        data, tmp_path, "myLayout", StepData({}), "abc123", default_file_format="svg"
-    )
-    assert g.file_format == "svg"
-    data = {"!FILEFORMAT!": "pdf"}
-    g = MyMapGenerator(data, tmp_path, "myLayout", StepData({}), "abc123")
-    assert g.file_format == "pdf"
-    data = {"!FILEFORMAT!": "png"}
-    g = MyMapGenerator(data, tmp_path, "myLayout", StepData({}), "abc123")
-    assert g.file_format == "png"
-    data = {"!FILEFORMAT!": "PNG"}
-    g = MyMapGenerator(data, tmp_path, "myLayout", StepData({}), "abc123")
-    assert g.file_format == "png"
-    with pytest.raises(ValueError):
-        data = {"!FILEFORMAT!": "bmp"}
-        g = MyMapGenerator(data, tmp_path, "myLayout", StepData({}), "abc123")
+def test_get_print_layout(g: MapGenerator):
+    assert isinstance(g._get_print_layout(), QgsPrintLayout)
+
+
+def test_get_map_layer(g: MapGenerator):
+    with pytest.raises(ValueError, match="Could not find layer"):
+        g._get_map_layer("not existing layer name")
+    with pytest.raises(
+        ValueError, match="Found multiple .* layers with name ambiguous name."
+    ):
+        g._get_map_layer("ambiguous name")
+    assert isinstance(g._get_map_layer("poly"), QgsMapLayer)
+
+
+def test_get_all_map_layers(g: MapGenerator):
+    assert all(isinstance(x, QgsMapLayer) for x in g._get_all_map_layers())
+
+
+def test_set_map_layer_filter_expression(g: MapGenerator):
+    assert g._get_map_layer("poly").subsetString() != "name = 'poly2'"
+    g._set_map_layer_filter_expression("poly", "name = 'poly2'")
+    assert g._get_map_layer("poly").subsetString() == "name = 'poly2'"
+
+
+def test_remove_map_layer_filter_expression(g: MapGenerator):
+    assert g._get_map_layer("poly").subsetString() == "\"name\" = 'poly1'"
+    g._remove_map_layer_filter_expression("poly")
+    assert g._get_map_layer("poly").subsetString() == ""
+
+
+def test_set_map_layer_visibility(g: MapGenerator):
+    layer = g._get_map_layer("poly")
+    root = g.step_data.project.layerTreeRoot()  # type: ignore
+    node = root.findLayer(layer.id())
+    assert bool(node.itemVisibilityChecked())
+
+    g._set_map_layer_visibility("poly", False)
+    assert not bool(node.itemVisibilityChecked())
+
+
+def test_set_layer_labels_visibility(g: MapGenerator):
+    layer = g._get_map_layer("poly")
+    g._set_layer_labels_visibility("poly", True)
+    assert bool(layer.labelsEnabled())
+    g._set_layer_labels_visibility("poly", False)
+    assert not bool(layer.labelsEnabled())
+
+
+def test_set_layer_style(g: MapGenerator):
+    layer = g._get_map_layer("poly")
+    assert layer.styleManager().currentStyle() == "default"
+    g._set_layer_style("poly", "style42")
+    assert layer.styleManager().currentStyle() == "style42"
+
+
+def test_zoom_map_to_layer_extent(g: MapGenerator):
+    assert round(g.step_data.layout.itemById("Map 1").scale()) == 164224
+    g._zoom_map_to_layer_extent("Map 1", g._get_map_layer("poly"))
+    assert round(g.step_data.layout.itemById("Map 1").scale()) == 50035
+    g._zoom_map_to_layer_extent("Map 1", g._get_map_layer("poly"), buffer=100)
+    assert round(g.step_data.layout.itemById("Map 1").scale()) == 90244673
+    g._zoom_map_to_layer_extent("Map 1", g._get_map_layer("poly"), relative_buffer=0.1)
+    assert round(g.step_data.layout.itemById("Map 1").scale()) == 55038
+
+
+def test_scale_map_to_layer_extent(g: MapGenerator):
+    assert round(g.step_data.layout.itemById("Map 1").scale()) == 164224
+    g._scale_map_to_layer_extent("Map 1", g._get_map_layer("poly"))
+    assert round(g.step_data.layout.itemById("Map 1").scale()) == 1000
+    g._scale_map_to_layer_extent("Map 1", g._get_map_layer("poly"), scale=2000)
+    assert round(g.step_data.layout.itemById("Map 1").scale()) == 2000
+
+
+@pytest.mark.parametrize("export_format", ["pdf", "PDF", "png", "PNG", "svg", "SVG"])
+def test_export_print_layout(tmp_path, export_format):
+    DATA["!FILEFORMAT!"] = export_format
+    g = MyMapGenerator(DATA, tmp_path, "myLayout", StepData({}), "abc123")
+    assert not (tmp_path / Path(g.filename).name).exists()
+    g._export_print_layout()
+    assert (tmp_path / Path(g.filename).name).exists()
