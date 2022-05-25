b97f850e14ec3f81bfafb9b069a721894fe28f4e
Roland Lukesch <roland.lukesch@its-viennaregion.at>
2022-05-25 13:48:06 +0200

    Set version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="automaps",
-    version="1.0.0",
+    version="1.0.1",
     author="Roland Lukesch",
     author_email="roland.lukesch@its-viennaregion.at",
     description="Automatically generate customized and ready to print maps",
